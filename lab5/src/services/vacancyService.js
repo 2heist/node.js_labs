632f5ae195e30e2ccd237c3e4ac13840df6d0823
Kyrylo Bohutskyi <k.bogutskiu@gmail.com>
2026-05-20 02:21:26 +0300

    added duplicateVacancy method

--- lab5/src/services/vacancyService.js
+++ lab5/src/services/vacancyService.js
@@ -1,6 +1,8 @@
-//старий шаблон з лаб 4
-
 const vacancyRepository = require("../repositories/vacancieRepository");
+
+const sequelize = require("../../database/db");
+const Vacancy = require("../models/Vacancy");
+const Requirement = require("../models/Requirement");
 
 class VacancyService {
   async getAllVacancies(keyword) {
@@ -39,6 +41,51 @@ class VacancyService {
 
   async deleteVacancy(id) {
     return await vacancyRepository.delete(id);
+  }
+
+  async duplicateVacancy(id) {
+    const t = await sequelize.transaction();
+    try {
+      const vacancy = await Vacancy.findByPk(id, {
+        include: [Requirement],
+        transaction: t
+      });
+      if (!vacancy) throw new Error("Вакансію не знайдено");
+
+      const duplicatedVacancy = await Vacancy.create(
+        {
+          title: `${vacancy.title} (Copy)`,
+          company: vacancy.company,
+          salary: vacancy.salary,
+          description: vacancy.description
+        },
+        { transaction: t }
+      );
+
+      if (!vacancy.Requirements || vacancy.Requirements.length === 0) {
+        throw new Error(
+          "Неможливо дублювати вакансію без вимог"
+        );
+      }
+
+      const requirementsToCreate =
+        vacancy.Requirements.map(req => ({
+          text: req.text,
+          vacancyId: duplicatedVacancy.id
+        }));
+
+      await Requirement.bulkCreate(
+        requirementsToCreate,
+        { transaction: t }
+      );
+
+      await t.commit();
+      return duplicatedVacancy;
+
+    } catch (error) {
+        await t.rollback();
+        throw error;
+      }
   }
 }
 
