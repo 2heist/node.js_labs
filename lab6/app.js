cea857cf5351eaf65cd9be3368adb8560c96845d
Kyrylo Bohutskyi <k.bogutskiu@gmail.com>
2026-05-20 09:18:10 +0300

    created apiErrorHandler

--- lab6/app.js
+++ lab6/app.js
@@ -6,15 +6,18 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const userRoutes = require("./src/routes/userRoutes");
 const employerRoutes = require("./src/routes/employerRoutes");
+const vacancyApiRoutes = require("./src/routes/api/vacancyApiRoutes");
+const apiErrorHandler = require("./src/middlewares/apiErrorHandler");
 
 app.use("/", userRoutes);
 app.use("/", employerRoutes);
+app.use("/api/vacancies", vacancyApiRoutes);
 
-// перевірка підключення
 sequelize.authenticate()
   .then(() => {
     console.log("Sequelize connected");
@@ -23,11 +26,10 @@ sequelize.authenticate()
     console.error("Sequelize connection error:", err);
   });
 
-// sync таблиць
 sequelize.sync({ alter: true })
   .then(() => {
     console.log("Tables synced");
-
+    app.use("/api", apiErrorHandler);
     app.listen(3000, () => {
       console.log("Server running on http://localhost:3000");
     });
